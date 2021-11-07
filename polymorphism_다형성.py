e7d90e21bbc56735f3bc64f8c7cc3bbe28fad7c7
ShinGwangCheon <58453569+Shin-GC@users.noreply.github.com>
2021-11-08 01:01:11 +0900

    Polymorphism Practice case 추가

--- polymorphism_다형성.py
+++ polymorphism_다형성.py
@@ -1,7 +1,17 @@
 from math import pi
 
 
-class Rectangle:
+class Shape:
+    def area(self):
+        """도형의 넓이를 리턴 : 자식 클래스가 오버라이딩할 것"""
+        pass
+
+    def perimeter(self):
+        """도형의 둘레를 리턴 : 자식 클래스가 오버라이딩할 것"""
+        pass
+
+
+class Rectangle(Shape):
     def __init__(self, height, width):
         self.height = height
         self.width = width
@@ -16,7 +26,7 @@ class Rectangle:
         return f"밑변 : {self.width}, 높이 : {self.height} 인 직사각형"
 
 
-class Circle:
+class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
 
@@ -30,33 +40,51 @@ class Circle:
         return f"반지름 : {self.radius} 인 원"
 
 
+class Cylinder:
+    """원통 클래스"""
+    def __init__(self, radius, height):
+        self.radius = radius
+        self.height = height
+
+    def __str__(self):
+        """원통의 정보를 문자열로 리턴"""
+        return f"밑면 반지름 : {self.radius}, 높이 : {self.height} 인 원기둥"
+
+
 class Paint:
     def __init__(self):
-        self.shape = []
+        self.shapes = []
 
     def add_shape(self, shape):
-        self.shape.append(shape)
+        if isinstance(shape, Shape):  # shape 에 들어온 값이 Shape 값을 상속하고 있는지 확인
+            self.shapes.append(shape)
+        else:
+            print("넓이, 둘레를 구하는 메소드가 없는 도형은 추가할 수 없습니다.")
 
     def total_area_of_shapes(self):
-        return sum([shape.area() for shape in self.shape])
+        return sum([shape.area() for shape in self.shapes])
 
     def total_perimeter_of_shapes(self):
-        return sum([shape.perimeter() for shape in self.shape])
+        return sum([shape.perimeter() for shape in self.shapes])
 
     def __str__(self):
         res_str = "그림판 내부 도형들 \n"
-        for shape in self.shape:
+        for shape in self.shapes:
             res_str += str(shape) + "\n"
         return res_str
 
 
-rectangle = Rectangle(5, 7)
-circle = Circle(3)
-paint = Paint()
+cylinder = Cylinder(7, 4)
+rectangle = Rectangle(3, 7)
+circle = Circle(4)
 
-paint.add_shape(rectangle)
-paint.add_shape(circle)
+paint_program = Paint()
+paint_program.add_shape(cylinder)
+paint_program.add_shape(rectangle)
+paint_program.add_shape(circle)
 
-print(paint.total_area_of_shapes())
-print(paint.total_perimeter_of_shapes())
-print(paint)
+print(paint_program.total_area_of_shapes())
+print(paint_program.total_perimeter_of_shapes())
+
+
+
